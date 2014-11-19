a8def004e8702c24dafeabcda795329211fc4f5d
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2014-11-19 19:38:09 +0100

    cookie js updated with amd support

--- cookie.js
+++ cookie.js
@@ -1,126 +1,144 @@
 /**
- *  CookieJS - Just another small JavaScript library. Use this if you need to set/get/delete cookies.
+ *  CookieJS - Just another small JavaScript library.
+ *  Use this if you need to set/get/delete cookies.
  *  Website: http://www.codephun.com
  *  Twitter: http://www.twitter.com/bjarneo_
  *  Copyright (c) 2013, Bjarne Øverli
- *  Licensed under the MIT license,
+ *  Licensed under the MIT license
  *  http://www.opensource.org/licenses/MIT
  */
-
-var CookieJS =
-{
-    /**
-     * Set cookie.
-     * @param params.name - cookie name
-     * @param params.value - cookie value
-     * @param params.expires - Expires (days)
-     * @param params.path - Set path. '/' for the whole website.
-     * @param params.domain - Set domain
-     * @param params.secure - Use SSL when sending the cookie to the server
-     * @param params.httpOnly -  Using the HttpOnly flag when generating a cookie helps mitigate the risk of client side script accessing the protected cookie (if the browser supports it). (https://www.owasp.org/index.php/HttpOnly)
-     */
-    set: function(params) {
-        var cookie = params.name + '=' + encodeURI(params.value) + ';';
-        
-        if (params.expires) {
-            params.expires = new Date(new Date().getTime() + parseInt(params.expires) * 1000 * 60 * 60 * 24);
-            cookie += 'expires=' + params.expires.toUTCString() + ";";
-        }
-
-        cookie += (params.path) ? 'path=' + params.path + ';' : '';
-        cookie += (params.domain) ? 'domain=' + params.domain + ';' : '';
-        cookie += (params.secure) ? 'secure;' : '';
-        cookie += (params.httpOnly) ? 'httpOnly;' : '';
-
-        document.cookie = cookie;
-    },
-
-    /**
-     * Get cookie
-     * @param name - cookie name
-     * @returns value - cookie value
-     */
-    get: function(name) {
-        var parts = document.cookie.split(name + '=');
-        
-        if (parts.length == 2) {
-            return decodeURI(parts.pop().split(';').shift());
-        }
-    },
-
-    /**
-     * Get all cookies (if any)
-     * @returns object cookies
-     */
-    getAll: function () {
-        var cookies = {},
-            allCookies = document.cookie;
-
-        if (allCookies === '') {
+(function () {
+    'use strict';
+    
+    var CookieJS = {
+        /**
+         * Set cookie.
+         * @param params.name - cookie name
+         * @param params.value - cookie value
+         * @param params.expires - Expires (days)
+         * @param params.path - Set path. '/' for the whole website.
+         * @param params.domain - Set domain
+         * @param params.secure - Use SSL when sending the cookie to the server
+         * @param params.httpOnly -  Using the HttpOnly flag when generating a cookie helps
+                mitigate the risk of client side script accessing the protected cookie (if
+                the browser supports it). (https://www.owasp.org/index.php/HttpOnly)
+         */
+        set: function (params) {
+            var cookie = params.name + '=' + encodeURI(params.value) + ';';
+            
+            if (params.expires) {
+                params.expires = new Date(new Date().getTime() +
+                    parseInt(params.expires, 10) * 1000 * 60 * 60 * 24);
+                
+                cookie += 'expires=' + params.expires.toUTCString() + ";";
+            }
+    
+            cookie += (params.path) ? 'path=' + params.path + ';' : '';
+            cookie += (params.domain) ? 'domain=' + params.domain + ';' : '';
+            cookie += (params.secure) ? 'secure;' : '';
+            cookie += (params.httpOnly) ? 'httpOnly;' : '';
+    
+            document.cookie = cookie;
+        },
+    
+        /**
+         * Get cookie
+         * @param name - cookie name
+         * @returns value - cookie value
+         */
+        get: function (name) {
+            var parts = document.cookie.split(name + '=');
+            
+            if (parts.length === 2) {
+                return decodeURI(parts.pop().split(';').shift());
+            }
+            
+            return false;
+        },
+    
+        /**
+         * Get all cookies (if any)
+         * @returns object cookies
+         */
+        getAll: function () {
+            var cookies = {},
+                allCookies = document.cookie;
+    
+            if (allCookies === '') {
+                return cookies;
+            }
+    
+            var list = allCookies.split('; '),
+                len = list.length;
+    
+            while (len--) {
+                var cookie = list[len].split('=');
+                cookies[cookie[0]] = decodeURI(cookie[1]);
+            }
+    
             return cookies;
-        }
-
-        var list = allCookies.split('; '),
-            len = list.length;
-
-        while (len--) {
-            var cookie = list[len].split('=');
-            cookies[cookie[0]] = decodeURI(cookie[1]);
-        }
-
-        return cookies;
-    },
-
-    /**
-     * Get all keys
-     * @returns array keys
-     */
-    keys: function() {
-        var keys = [],
-            allCookies = document.cookie;
-
-        if (allCookies === '') {
+        },
+    
+        /**
+         * Get all keys
+         * @returns array keys
+         */
+        keys: function () {
+            var keys = [],
+                allCookies = document.cookie;
+    
+            if (allCookies === '') {
+                return keys;
+            }
+    
+            var list = allCookies.split('; '),
+                len = list.length;
+    
+            while (len--) {
+                var key = list[len].split('=');
+                keys[len] = key[0];
+            }
+    
             return keys;
+        },
+    
+        /**
+         * Check if cookie exists
+         * @param name - cookie name
+         */
+        has: function (name) {
+            if (this.get(name)) {
+                return true;
+            }
+    
+            return false;
+        },
+    
+        /**
+         * Delete cookie
+         * @param params.name - cookie name
+         * @param params.path - cookie path
+         * @param params.domain - cookie domain
+         */
+        del: function (params) {
+            if (this.get(params.name)) {
+                this.set({
+                    name: params.name,
+                    value: '',
+                    expires: -1,
+                    path: params.path,
+                    domain: params.domain
+                });
+            }
         }
-
-        var list = allCookies.split('; '),
-            len = list.length;
-
-        while (len--) {
-            var key = list[len].split('=');
-            keys[len] = key[0];
-        }
-
-        return keys;
-    },
-
-    /**
-     * Check if cookie exists
-     * @param name - cookie name
-     */
-    has: function(name) {
-        if (this.get(name)) {
-            return true;
-        }
-
-        return false;
-    },
-
-    /**
-     * Delete cookie
-     * @param params.name - cookie name
-     * @param params.path - cookie path
-     * @param params.domain - cookie domain
-     */
-    delete: function(params) {
-        if (this.get(params.name)) {
-            this.set({
-                name: params.name,
-                value: '',
-                expires: -1,
-                path: params.path,
-                domain: params.domain
-            });
-        }
+    };
+    
+    // Check if is node, amd or else we set it to window
+    if (typeof define === 'function' && define.amd) {
+        define([], function() {
+                return CookieJS;
+        });
+    } else {
+        window.CookieJS = CookieJS;
     }
-};
+})();
